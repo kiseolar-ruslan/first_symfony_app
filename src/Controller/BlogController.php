541c87d5661b50cf7c6d90a3eb8d76b894127692
kiseolar-ruslan <kretsu.rus@gmail.com>
2023-07-21 02:38:26 +0300

    training

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,14 +3,30 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
 //    #[Route('/blog', name: 'app_blog')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        // Getting a route name
+        var_dump($routeName = $request->attributes->get('_route'));
+        echo "<br>";
+
+        // Getting a route parameters
+        var_dump($routeParameters = $request->attributes->get('_route_params'));
+        echo "<br>";
+
+        $allAttributes = $request->attributes->all();
+        foreach ($allAttributes as $allAttribute) {
+            echo "<br>";
+            var_dump($allAttribute);
+            echo "</br>";
+        }
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
         ]);
